9b66740e24e24cb0c7c306b14b95d9ed69fa841a
Valery Zubkov <bouddha@mail.ru>
2022-08-15 15:14:35 +0400

    homework number 2

---
+++ Homework_002/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задать массив из 12 элементов, заполненных числами из [0,9]. 
+//Найти сумму положительных/отрицательных элементов массива
+Console.WriteLine("Дан массив: ");
+int[] array = new int[12];
+int sumPositiv = 0;
+int sumNegativ = 0;
+for(int i = 0; i < 12; i++)
+{
+    array[i] = new Random().Next(-9, 10);
+    Console.WriteLine(array[i] + " ");
+    if(array[i] > 0)
+    {
+    sumPositiv = sumPositiv + array[i];
+    }
+    else
+    {
+    sumNegativ = sumNegativ + array[i];  
+    }  
+}
+Console.WriteLine("Сумма положительных элементов массива равно: " + sumPositiv);
+Console.WriteLine("Сумма отрицательных элементов массива равно: " + sumNegativ);
+
