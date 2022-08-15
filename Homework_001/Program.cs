d3cca1ea2d3454ee9df64d158697ff156331d167
Valery Zubkov <bouddha@mail.ru>
2022-08-15 14:47:22 +0400

    homework number 1

---
+++ Homework_001/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+int[] array = new int [8];
+for(int count = 0; count < 8; count++)
+{
+    array[count] = new Random().Next(0,2);
+    Console.WriteLine((count + 1) + "-й элемент = " + array[count]);
+}
