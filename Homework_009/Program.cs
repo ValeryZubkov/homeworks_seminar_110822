c14acdeb84c8054b88054b55d4d604296d755406
Valery Zubkov <bouddha@mail.ru>
2022-08-16 22:05:07 +0400

    homework number 9

---
+++ Homework_009/Program.cs
@@ -0,0 +1,19 @@
+﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+int size = new Random().Next(5,20);
+double[] array = new double[size];
+Console.WriteLine("В массиве:");
+for(int i = 0; i < size; i++)
+{
+    array[i] = new Random().NextDouble() * 100;    
+    Console.Write($"{array[i].ToString("0.00")}  ");
+}
+Console.WriteLine();
+double min = array[0];
+double max = array[0];
+for(int i = 0; i < size; i++)
+{
+   if(array[i] < min) min = array[i];
+   if(array[i] > max) max = array[i];
+}
+double result = max - min;
+Console.WriteLine($"разница между максимальным ({max.ToString("0.00")}) и минимальным ({min.ToString("0.00")}) элементом составляет: {result.ToString("0.00")}.");
