9cefc1117a9136e571b536abb96e142e28ad1b67
Valery Zubkov <bouddha@mail.ru>
2022-08-16 19:40:48 +0400

    homework number 7

---
+++ Homework_007/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+int size = new Random().Next(5,11);
+int min = 0; 
+int max = 100;
+int[] array = new int[size];
+int sum = 0;
+Console.WriteLine("В массиве:");
+for(int i = 0; i<size; i++)
+{
+    array[i] = new Random().Next(min, max+1);
+    if(i%2 != 0) sum = sum + array[i]; 
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+Console.WriteLine($"сумма нечетных элементов равна {sum}.");
